d9734842fe99fe42eee42c54ff36166131655fb9
Adrian <adrian.mael.jung@gmail.com>
2023-05-05 21:30:22 +0200

    work on tictactoe

--- TicTacToe/src/App.tsx
+++ TicTacToe/src/App.tsx
@@ -1,25 +1,38 @@
-function MyButton() {
-	return <button>Je suis un boutton</button>;
+type Props = {
+	value1: string;
+	value2: string;
+};
+
+function Square({ value1, value2 }: Props) {
+	return <button className="square">{value1 + value2}</button>;
 }
 
-function AboutPage() {
+function Board() {
 	return (
-		<div>
-			<h1>About</h1>
-			<p>
-				Hello there. <br />
-				How are you doing ?
-			</p>
-		</div>
+		<>
+			<div className="board-row">
+				<Square value1={"0"} value2={"1"} />
+				<Square value="2" />
+				<Square value="3" />
+			</div>
+			<div className="board-row">
+				<Square value="4" />
+				<Square value="5" />
+				<Square value="6" />
+			</div>
+			<div className="board-row">
+				<Square value="7" />
+				<Square value="8" />
+				<Square value="9" />
+			</div>
+		</>
 	);
 }
 
 function App() {
 	return (
 		<div>
-			<h1>Welcome to my app</h1>
-			<MyButton />
-			<AboutPage />
+			<Board />
 		</div>
 	);
 }
