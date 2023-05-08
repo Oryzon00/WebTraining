e522992b780960c17d5bfc85c3a68b60ed2045c5
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-05-08 19:10:51 +0200

    Tictactoe winner/equality detection

--- TicTacToe/src/App.tsx
+++ TicTacToe/src/App.tsx
@@ -1,12 +1,45 @@
 import { useState } from "react";
 
 interface SquareProps {
-	value: "X" | "O" | "";
+	value: Input;
 	onSquareClick: () => void;
 }
 
 interface ResetButtonProps {
 	onButtonClick: () => void;
+}
+
+interface StatusProps {
+	winner: Input | "equality";
+}
+
+type Input = "X" | "O" | "";
+
+function checkWinner(squares: Array<Input>): Input | "equality" {
+	const lines: Array<Array<number>> = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+
+	for (let i: number = 0; i < lines.length; i++) {
+		const [a, b, c]: number[] = lines[i];
+		if (
+			squares[a] != "" &&
+			squares[a] === squares[b] &&
+			squares[b] == squares[c]
+		)
+			return squares[a];
+	}
+
+	if (!squares.includes("")) return "equality";
+
+	return "";
 }
 
 function Square({ value, onSquareClick }: SquareProps) {
@@ -21,9 +54,15 @@ function ResetButton({ onButtonClick }: ResetButtonProps) {
 	return <button onClick={onButtonClick}> RESET </button>;
 }
 
+function Status({ winner }: StatusProps) {
+	if (winner === "") return <h2> No winner... yet?</h2>;
+	else if (winner === "equality") return <h2> Equality! </h2>;
+	else return <h2> {winner} won ! Congratz! </h2>;
+}
+
 function Board() {
 	const [xTurn, setXTurn] = useState(true);
-	const [squares, setSquares] = useState(Array<"X" | "O" | "">(9).fill(""));
+	const [squares, setSquares] = useState(Array<Input>(9).fill(""));
 
 	function handleReset(): void {
 		const nextSquares = squares.slice();
@@ -32,17 +71,20 @@ function Board() {
 	}
 
 	function handleClick(index: number): void {
-		if (squares[index] !== "") return;
+		if (squares[index] !== "" || checkWinner(squares) !== "") return;
+
 		const nextSquares = squares.slice();
 		if (xTurn === true) nextSquares[index] = "X";
 		else nextSquares[index] = "O";
+
 		setXTurn(!xTurn);
 		setSquares(nextSquares);
 	}
 
+		//faire ca en boucle for
 	return (
 		<>
-			<div className="board-row">
+			<div className="board-row"> 
 				<Square
 					value={squares[0]}
 					onSquareClick={() => handleClick(0)}
@@ -87,6 +129,20 @@ function Board() {
 			<div>
 				<ResetButton onButtonClick={() => handleReset()} />
 			</div>
+			<div>
+				<Status winner={checkWinner(squares)} />
+			</div>
+		</>
+	);
+}
+
+function Game() {
+	useState()
+	return (
+		<>
+			<Board />
+			<ResetButton />
+			<Status />
 		</>
 	);
 }
